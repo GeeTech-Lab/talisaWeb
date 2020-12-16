08f3e8eeb31b83b5b074c62760c13c2668f676ab
Gerard-007 <gerardnwazk@gmail.com>
2020-12-16 09:16:02 -0800

    initial commit

--- talisa_app/urls.py
+++ talisa_app/urls.py
@@ -22,7 +22,7 @@ from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', TemplateView.as_view(template_name='index.html')),
+    path('', TemplateView.as_view(template_name='index.html'), name='home'),
 ]
 
 # url to catch any unmatch url for 404...
